2bc19845b16cccfaa4f8aad048fd052ceb12bdf1
stevo9061 <sbittgen1@gmail.com>
2024-10-24 18:14:23 +0200

    integrated backend api

--- Use-Case-2/frontend/crypto-dashboard-frontend/src/app/crypto-table/crypto-table.component.ts
+++ Use-Case-2/frontend/crypto-dashboard-frontend/src/app/crypto-table/crypto-table.component.ts
@@ -3,7 +3,8 @@ import { TableModule } from 'primeng/table';
 import { PaginatorModule } from 'primeng/paginator';
 import { CommonModule } from '@angular/common'
 import { ButtonModule } from 'primeng/button';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Cryptocurrency } from '../models/cryptocurrency.model';
 
 @Component({
   selector: 'app-crypto-table',
@@ -17,17 +18,29 @@ export class CryptoTableComponent {
   first = 0;
   rows = 20;
 
+  constructor(private http: HttpClient) {}
+
   ngOnInit() {
-    this.loadCryptocurrencies( { first: 0, row: 20 });
+    this.loadCryptocurrencies()
   }
 
-  loadCryptocurrencies(even: any) {
-    // Load cryptocurrencies from the backend
-
+  loadCryptocurrencies() {
+    // fetch data from the API
+    this.http.get<Cryptocurrency[]>('http://localhost:8080/api/crypto/fetch-and-save')
+      .subscribe({
+        next: (data: Cryptocurrency[]) => {
+          this.cryptocurrencies = data; // fetch data from api into the table
+        },
+        error: (error: any) => {
+          console.error('Error fetching cryptocurrencies', error);
+        }
+      });
+/* 
     this.cryptocurrencies = [
       { id: 1, coin: 'BTC', name: 'Bitcoin', supply: 192345, maxSupply: 21000000, marketCap: 132034543, volumeUsd24Hr: 13452345, price: 66817, changePercent24Hr: 1.8, vwap24Hr: 66080, explorer: 'https://lalelu.at' },
       { id: 2, coin: 'ETH', name: 'Ethereum', supply: 192345, maxSupply: 21000000, marketCap: 132034543, volumeUsd24Hr: 13452345, price: 66817, changePercent24Hr: 1.8, vwap24Hr: 66080, explorer: 'https://lalelu.at' },
-    ];
+    ]; */
+
   }
 
   next() {
