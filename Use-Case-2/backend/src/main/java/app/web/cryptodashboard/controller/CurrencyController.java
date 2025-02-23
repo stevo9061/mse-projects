06daa9d5e7fa6e06141fbd5d6ea3002289b2ca0a
stevo9061 <sbittgen1@gmail.com>
2025-02-23 15:54:59 +0100

    forgot one slash for request mapping in currency controller

--- Use-Case-2/backend/src/main/java/app/web/cryptodashboard/controller/CurrencyController.java
+++ Use-Case-2/backend/src/main/java/app/web/cryptodashboard/controller/CurrencyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/currencies")
+@RequestMapping("/api/currencies")
 @CrossOrigin
 public class CurrencyController {
 
